3af6101a8f0ba6b4c4c74095c9ace1226fd9f603
edipojuan <edipojs@gmail.com>
2019-12-01 11:01:23 -0400

    Adicionado a propriedade CreatedAt

--- SGE-API/src/SGE.UI.Web/Models/Evento.cs
+++ SGE-API/src/SGE.UI.Web/Models/Evento.cs
@@ -14,5 +14,7 @@ namespace SGE.UI.Web.Models
     public string Author { get; set; }
     public DateTime Date { get; set; }
     public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+
   }
 }
