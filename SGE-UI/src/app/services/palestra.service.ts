bd24bf0e640a1fd5b7d49c7c131284bc457c5899
edipojuan <edipojs@gmail.com>
2019-12-08 14:47:35 -0400

    Extendido ServiceBase no PalestraService

--- SGE-UI/src/app/services/palestra.service.ts
+++ SGE-UI/src/app/services/palestra.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { ServiceBase } from '../shared/base/service.base';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PalestraService {
+export class PalestraService extends ServiceBase {
+  constructor(injector: Injector) {
+    super(injector, 'palestra');
+  }
 
-  constructor() { }
+  find = (body: any) => this.get('consulta-simples');
 }
